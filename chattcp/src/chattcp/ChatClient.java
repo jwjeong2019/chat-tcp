c4443933522cab2fb8760c5da7bbd98fac335e8a
jwjeong <timestar7979@gmail.com>
2025-03-16 22:37:09 +0900

    fix: SocketException - Socket closed

--- chattcp/src/chattcp/ChatClient.java
+++ chattcp/src/chattcp/ChatClient.java
@@ -10,7 +10,8 @@ public class ChatClient {
 	private static final int SERVER_PORT = 9000;
 	
 	public static void main(String[] args) {
-		try (Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
+		try {
+			Socket socket = new Socket(SERVER_IP, SERVER_PORT);
 			ClientMainThread mainThread = new ClientMainThread(socket);
 			mainThread.init();
 			mainThread.start();
